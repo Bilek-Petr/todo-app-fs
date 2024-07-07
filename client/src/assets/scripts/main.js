ff81a116bd50f13f456588b77f37b6661f6398b6
Bilek-Petr <p.bilek29@gmail.com>
2024-07-07 20:29:44 +0200

    Updates

--- client/src/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import './modules/utility.js';
-import './todo-actions.js';
-import './api.js';
-import './states.js';
-
-import { toggleTheme } from './modules/theme.js';
-
-const themeBtn = document.querySelector('.header__btn-theme');
-themeBtn.addEventListener('click', toggleTheme);
